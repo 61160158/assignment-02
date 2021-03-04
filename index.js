916eb31b3b689d7e9dd94b546395fc8ce3997893
Poonsak Wijitwattana <61160158@go.buu.ac.th>
2021-03-04 19:11:28 +0700

    #3

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app = express()
 app.use(express.json())
 let books = []
 
+app.get('/books/:id', async (req, res) => {
+    let id = req.params.id
+
+    res.status(200).json(books[id])
+})
+
 app.post('/books', async (req, res) => {
     let newTitle = req.body.title
     let newPrice = req.body.price
     let newUnit = req.body.unit
     let newIsbn = req.body.isbn
-    let newImage = req.body.image
+    let newImage = req.body.image_url
 
 
     let newBook = {
